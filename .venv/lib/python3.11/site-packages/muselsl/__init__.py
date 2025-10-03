9aac187df067d09973b7888aeb5c4c13794e5cb0
symbox <nathansen.nh@gmail.com>
2025-10-03 14:29:34 +0800

    Stop tracking .venv folder

--- .venv/lib/python3.11/site-packages/muselsl/__init__.py
+++
@@ -1,4 +0,0 @@
-from .stream import stream, list_muses
-from .record import record, record_direct
-from .view import view
-__version__ = "2.3.1"
--- .venv/lib/python3.11/site-packages/pygatt/backends/__init__.py
+++
@@ -1,3 +0,0 @@
-from .backend import BLEBackend, Characteristic, BLEAddressType  # noqa
-from .bgapi.bgapi import BGAPIBackend  # noqa
-from .gatttool.gatttool import GATTToolBackend  # noqa
